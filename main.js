50dc39b3733bbe323d451aba07db954c7833c11c
Tobias-soderberg <145038029+Tobias-soderberg@users.noreply.github.com>
2024-01-24 18:15:30 +0100

    Added update and delete, also comments and nicer look

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-//Change this to your URL:
+// Change this to your API endpoint URL
 var url = "http://localhost:5174/Moves";
 
-//Selectors
+// SELECTORS
 let moviesContainer = document.querySelector("#movies-container");
 let idInput = document.querySelector("#id-input");
 let titleInput = document.querySelector("#title-input");
@@ -9,14 +9,22 @@ let genreInput = document.querySelector("#genre-input");
 let dateInput = document.querySelector("#date-input");
 let imgInput = document.querySelector("#img-input");
 let addMovieBtn = document.querySelector("#add-movie-btn");
+let errorText = document.getElementById("error-message");
 
-//Event listeners
-
+// EVENT LISTENERS
 addMovieBtn.addEventListener("click", addMovie);
 
-//Functions
+// --------- FUNCTIONS ---------
 
+//--------- ADD MOVIES ---------
+// Function to add a new movie
 function addMovie() {
+  // Validate input fields
+  if (!validateInputs()) {
+    return;
+  }
+
+  // Create a new movie object
   let newMovie = {
     id: Number(idInput.value),
     title: titleInput.value,
@@ -25,6 +33,7 @@ function addMovie() {
     imageLink: imgInput.value,
   };
 
+  // Send a POST request to add the new movie
   fetch(url, {
     method: "POST",
     headers: {
@@ -33,6 +42,7 @@ function addMovie() {
     body: JSON.stringify(newMovie),
   }).then((res) => {
     if (res.ok) {
+      // If successful, refresh the displayed movies
       getMovies();
     } else {
       console.warn("Something is wrong with the API!");
@@ -40,29 +50,177 @@ function addMovie() {
   });
 }
 
+//--------- GET MOVIES ---------
+// Function to retrieve and display movies
 function getMovies() {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayMovies(data));
+    .then((data) => displayMovies(data))
+    .then(() => {
+      // Add event listeners to update and delete buttons
+      addBtnListeners();
+    });
 }
 
+//--------- DISPLAY MOVIES ---------
+// Function to display movies in the HTML
 function displayMovies(movies) {
   moviesContainer.innerHTML = "";
 
   movies.forEach((movie) => {
+    // Create HTML for each movie
     var html = `
-    <div class="sm-col-12 md-col-6 lg-col-4">
-        <h1 class="text-center">${movie.title}</h1>
-        <h3>${movie.releaseYear}</h3>
-        <img src="${movie.imageLink}">
-        <p><span class="bold-text">Id: </span>${movie.id}</p>
-        <p><span class="bold-text">Genre: </span>${movie.genre}</p>
-
-    </div>
-    `;
+      <div class="col-md-6 col-lg-4 movie-div">
+          <h1 class="text-center">${movie.title}</h1>
+          <h3 class="text-center">${movie.releaseYear}</h3>
+          <img class="img" src="${movie.imageLink}">
+          <p><span class="bold-text">Id: </span>${movie.id}</p>
+          <p><span class="bold-text">Genre: </span>${movie.genre}</p>
+          <div class="text-center">
+              <button type="button" class="btn btn-primary update-btn" data-movie-id="${movie.id}">Update</button>
+              <button type="button" class="btn btn-danger delete-btn" data-movie-id="${movie.id}">Delete</button>
+          </div>
+      </div>`;
     moviesContainer.innerHTML += html;
   });
 }
 
-//Display at start:
+// --------- ADD BTN LISTENERS ---------
+// Function to add event listeners to update and delete buttons
+function addBtnListeners() {
+  let updateButtons = document.querySelectorAll(".update-btn");
+  let deleteButtons = document.querySelectorAll(".delete-btn");
+
+  updateButtons.forEach((button) => {
+    button.addEventListener("click", handleUpdate);
+  });
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", handleDelete);
+  });
+}
+
+// --------- HANDLE UPDATE ---------
+// Function to handle the update button click
+function handleUpdate(event) {
+  const movieId = event.currentTarget.dataset.movieId;
+
+  // Confirm update with the user
+  if (
+    !window.confirm(
+      `Are you sure you want to update the movie with id ${movieId}?`
+    ) &&
+    !validateInputs()
+  ) {
+    return;
+  }
+
+  // Create updated movie object
+  let updatedMovieData = {
+    id: Number(idInput.value),
+    title: titleInput.value,
+    genre: genreInput.value,
+    releaseYear: Number(dateInput.value),
+    imageLink: imgInput.value,
+  };
+
+  // Construct the update URL
+  const updateUrl = url + "/" + movieId;
+
+  // Send a PUT request to update the movie
+  fetch(updateUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedMovieData),
+  })
+    .then((res) => {
+      if (res.ok) {
+        // If successful, refresh the displayed movies
+        getMovies();
+      } else {
+        console.warn("Something is wrong with the API!");
+      }
+    })
+    .catch((error) => {
+      console.error("Error during fetch:", error);
+    });
+}
+
+// // --------- HANDLE DELETE ---------
+// Function to handle the delete button click
+function handleDelete(event) {
+  const movieId = event.currentTarget.dataset.movieId;
+
+  // Confirm delete with the user
+  if (
+    !window.confirm(
+      `Are you sure you want to delete the movie with id ${movieId}?`
+    ) &&
+    !validateInputs()
+  ) {
+    return;
+  }
+
+  // Construct the delete URL
+  const deleteUrl = url + "/" + movieId;
+
+  // Send a DELETE request to delete the movie
+  fetch(deleteUrl, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      if (res.ok) {
+        // If successful, refresh the displayed movies
+        getMovies();
+      } else {
+        console.warn("Something is wrong with the API!");
+      }
+    })
+    .catch((error) => {
+      console.error("Error during fetch:", error);
+    });
+}
+
+// --------- VALIDATE INPUTS ---------
+// Function to validate input fields
+function validateInputs() {
+  // Check if all required fields are filled in
+  if (
+    !idInput.value ||
+    !titleInput.value ||
+    !genreInput.value ||
+    !dateInput.value
+  ) {
+    // Display an error message on the webpage
+    showError("Wrong inputs, check them and try again...");
+    return false;
+  }
+
+  // If image input is empty, set it to an empty string
+  if (!imgInput.value) {
+    imgInput.value = "";
+  }
+
+  // Clear any previous error messages
+  clearError();
+  return true;
+}
+
+// --------- SHOW ERROR MESSAGE ---------
+// Function to display error message on the webpage
+function showError(message) {
+  errorText.textContent = message;
+  errorText.style.display = "block";
+}
+
+// --------- CLEAR ERROR MESSAGE ---------
+// Function to clear error messages on the webpage
+function clearError() {
+  errorText.textContent = "";
+  errorText.style.display = "none";
+}
+
+// Display movies at the start
 getMovies();
